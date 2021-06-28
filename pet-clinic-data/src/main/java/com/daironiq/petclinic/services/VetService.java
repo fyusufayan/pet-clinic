20074529ab164332bb70aed4813ce6d1c642c735
fyusufayan <f.yusuf88@gmail.com>
2021-06-28 23:58:46 +0300

    refactoring to common crud interfaces. closes #11

--- pet-clinic-data/src/main/java/com/daironiq/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/daironiq/petclinic/services/VetService.java
@@ -2,13 +2,8 @@ package com.daironiq.petclinic.services;
 
 import com.daironiq.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
+public interface VetService extends CrudService<Vet,Long> {
 
-    Vet findById(Long id);
 
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
