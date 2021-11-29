50aaaa84602a4a4341501621b626f38dfa114150
fyusufayan <f.yusuf88@gmail.com>
2021-11-29 16:27:58 +0300

    update visits jpa entity. closes #38

--- pet-clinic-data/src/main/java/com/daironiq/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/daironiq/petclinic/model/Pet.java
@@ -3,6 +3,8 @@ package com.daironiq.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,18 @@ public class Pet extends BaseEntity{
 
     @Column(name = "birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+    private Set<Visit> visits=new HashSet<>();
+
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
