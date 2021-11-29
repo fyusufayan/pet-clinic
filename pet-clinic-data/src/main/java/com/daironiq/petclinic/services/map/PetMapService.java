66826e4d33bcba228bb64c1ae0ce2030253ae2b5
fyusufayan <f.yusuf88@gmail.com>
2021-11-29 23:06:46 +0300

    updated profiles. closes #48

--- pet-clinic-data/src/main/java/com/daironiq/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/daironiq/petclinic/services/map/PetMapService.java
@@ -2,11 +2,13 @@ package com.daironiq.petclinic.services.map;
 
 import com.daironiq.petclinic.model.Pet;
 import com.daironiq.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default","map"})
 public class PetMapService extends AbstractMapService<Pet,Long> implements PetService {
 
     public Set<Pet> findAll() {
