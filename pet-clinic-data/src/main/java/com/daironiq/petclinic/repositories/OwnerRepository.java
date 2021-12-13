306d66107366a5f2818ab5fa0ab03fa95082f693
fyusufayan <f.yusuf88@gmail.com>
2021-12-13 14:36:10 +0300

    impl find owner. closes #54

--- pet-clinic-data/src/main/java/com/daironiq/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/daironiq/petclinic/repositories/OwnerRepository.java
@@ -3,7 +3,11 @@ package com.daironiq.petclinic.repositories;
 import com.daironiq.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/daironiq/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/daironiq/petclinic/services/OwnerService.java
@@ -2,8 +2,12 @@ package com.daironiq.petclinic.services;
 
 import com.daironiq.petclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
